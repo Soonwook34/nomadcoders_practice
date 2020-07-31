068c0af11c7a4df0879ad512fea96f77f53834ad
soonwook34 <soonwook34@gmail.com>
2020-07-31 13:08:05 +0900

    change colors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 let painting = false;
 
@@ -23,6 +24,11 @@ function onMouseMove(event) {
     }
 }
 
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
 function init() {
     if (canvas) {
         canvas.addEventListener("mousemove", onMouseMove);
@@ -34,6 +40,9 @@ function init() {
         ctx.strokeStyle = "#2c2c2c";
         ctx.lineWidth = 2.5;
     }
+    Array.from(colors).forEach((color) =>
+        color.addEventListener("click", handleColorClick)
+    );
 }
 
 init();

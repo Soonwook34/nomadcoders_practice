3a31354700d3f4c3ebda39e00e63fc82a9b01870
soonwook34 <soonwook34@gmail.com>
2020-07-31 12:58:01 +0900

    drawing line

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
 function init() {
-    console.log("hello?");
+    if (canvas) {
+        canvas.addEventListener("mousemove", onMouseMove);
+        canvas.addEventListener("mousedown", startPainting);
+        canvas.addEventListener("mouseup", stopPainting);
+        canvas.addEventListener("mouseleave", stopPainting);
+        canvas.width = 700;
+        canvas.height = 700;
+        ctx.strokeStyle = "#2c2c2c";
+        ctx.lineWidth = 2.5;
+    }
 }
 
 init();
